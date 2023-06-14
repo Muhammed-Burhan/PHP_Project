7a59a955d34224958ce4ffb5250381853812253a
Muhammed-Burhan <muhammadburhan964@gmail.com>
2023-06-14 17:54:46 +0300

    DB:connect

--- index.php
+++ index.php
@@ -2,11 +2,26 @@
 
 require 'functions.php';
 
-require 'router.php';
+//require 'router.php';
+
+//connect to our Mysql database.
+
+$dsn="mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+
+$pdo=new PDO($dsn);
 
 
+    $statement=$pdo->prepare("SELECT * FROM posts;");
+    $statement->execute();
+    //we say fetch data as associative array
+    $posts=$statement->fetchAll(PDO::FETCH_ASSOC);
+;
+    
 
+    
 
-
+    foreach ($posts as $key=>$post) {
+        echo "<li>{$post['title']}</li>";
+    }
 
 ?>
