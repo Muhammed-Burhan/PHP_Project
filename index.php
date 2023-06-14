755d169ba2f06d976a34c8a31f9242391f820ee7
Muhammed-Burhan <muhammadburhan964@gmail.com>
2023-06-14 18:29:06 +0300

    Add:Database Class

--- index.php
+++ index.php
@@ -1,24 +1,14 @@
 <?php 
 
+require 'Database.php';
 require 'functions.php';
-
 //require 'router.php';
 
-//connect to our Mysql database.
 
-$dsn="mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+$db=new Database();
 
-$pdo=new PDO($dsn);
+$posts=$db->query("SELECT * FROM posts;")->fetchAll(PDO::FETCH_ASSOC);
 
-
-    $statement=$pdo->prepare("SELECT * FROM posts;");
-    $statement->execute();
-    //we say fetch data as associative array
-    $posts=$statement->fetchAll(PDO::FETCH_ASSOC);
-;
-    
-
-    
 
     foreach ($posts as $key=>$post) {
         echo "<li>{$post['title']}</li>";
